936c37c5894a674beb11433682a05c0e0c093101
David Tavares <davidtavaresfs@gmail.com>
2026-02-02 10:21:08 -0300

    refactor: Ajustes em classes Course e Mentoring

--- src/br/com/dio/desafio/domain/Course.java
+++ src/br/com/dio/desafio/domain/Course.java
@@ -1,4 +1,30 @@
 package br.com.dio.desafio.domain;
 
-public class Course {
+public class Course extends Content {
+
+    private int workload;
+
+    public Course() {}
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", workload=" + workload +
+                '}';
+    }
+
+    @Override
+    public double calcXp() {
+        return XP_DEFAULT * this.workload;
+    }
 }
--- src/br/com/dio/desafio/domain/Mentoring.java
+++ src/br/com/dio/desafio/domain/Mentoring.java
@@ -1,4 +1,33 @@
 package br.com.dio.desafio.domain;
 
-public class Mentoring {
+import java.time.LocalDate;
+
+public class Mentoring extends Content {
+
+    private LocalDate date;
+
+    public Mentoring() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoring{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    @Override
+    public double calcXp() {
+        return XP_DEFAULT + 10;
+    }
 }
